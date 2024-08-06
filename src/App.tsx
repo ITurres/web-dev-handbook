ddfda7289e015402c13c55fac3cc7e2926d2880b
ITurres <emanueliturres.co@gmail.com>
2024-08-05 21:46:36 -0300

    feat: Update 'App' component with new layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,60 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useMemo, useState } from 'react';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+
 import './App.scss';
 
+import Layout from './components/Layout/Layout';
+import BrowserStorageService from './services/browserStorage.service';
+
 function App() {
-  const [count, setCount] = useState(0);
+  const isUserFirstTimeStorageKey = 'isUserFirstTime';
+  const browserStorageService = useMemo(() => new BrowserStorageService(), []);
+
+  const [isUserFirstTime, setIsUserFirstTime] = useState<boolean>(true);
+
+  useEffect(() => {
+    const storedValue = browserStorageService.getLocalStorage<boolean>(
+      isUserFirstTimeStorageKey,
+    );
+
+    if (storedValue === null) {
+      browserStorageService.setLocalStorage(isUserFirstTimeStorageKey, true);
+      setIsUserFirstTime(true);
+    } else {
+      setIsUserFirstTime(storedValue);
+    }
+  }, [browserStorageService]);
+
+  const handleAccess = () => {
+    setIsUserFirstTime(false);
+    browserStorageService.setLocalStorage(isUserFirstTimeStorageKey, false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is
-          &nbsp;
-          {count}
-        </button>
-        <p>
-          Edit
-          &nbsp;
-          <code>
-            src/App.tsx
-          </code>
-          &nbsp;
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isUserFirstTime ? (
+        <div>
+          <h1>Welcome to Arthur&apos;s Web Dev Handbook</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+            aperiam sed consequuntur consectetur omnis eum voluptatibus
+            excepturi quaerat quae earum repellendus alias voluptate rem, dolor
+            minima atque explicabo ipsa aspernatur.
+          </p>
+
+          <button type="button" onClick={handleAccess}>
+            Access
+          </button>
+        </div>
+      ) : (
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<h1>Home</h1>} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      )}
     </>
   );
 }
