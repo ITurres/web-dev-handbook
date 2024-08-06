4f69187f96629f8f1ac040473058097d364cad63
ITurres <emanueliturres.co@gmail.com>
2024-08-05 21:44:17 -0300

    refactor: Update 'main.tsx'

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(
+  document.getElementById('root') || document.createElement('div'),
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
